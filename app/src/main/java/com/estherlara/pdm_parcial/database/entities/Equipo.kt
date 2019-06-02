85873507c0f07eb9a31feaae34b26326fb8ebf5c
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2019-06-01 18:15:08 -0600

    Creando las entidades

--- app/src/main/java/com/estherlara/pdm_parcial/database/entities/Equipo.kt
+++ app/src/main/java/com/estherlara/pdm_parcial/database/entities/Equipo.kt
@@ -1,4 +1,14 @@
 package com.estherlara.pdm_parcial.database.entities
 
-class Equipo {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Equipo (
+
+    @PrimaryKey
+    private val id: Int,
+
+    private val name: String,
+    private val points: Int
+)
--- app/src/main/java/com/estherlara/pdm_parcial/database/entities/Partido.kt
+++ app/src/main/java/com/estherlara/pdm_parcial/database/entities/Partido.kt
@@ -1,12 +1,19 @@
 package com.estherlara.pdm_parcial.database.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.sql.Time
 import java.util.*
 
-
+@Entity
 data class Partido (
+
+    @PrimaryKey
+    private val id: Int,
+
     private val teamA: String,
     private val teamB: String,
     private val date: Date,
-    private val time: Time
+    private val time: Time,
+    private val points: Int
 )
